910a3aa3ee2174e6e65c58967e194b1fb057bef0
rapinasa <rapina1990@gmail.com>
2018-11-24 23:51:47 +0200

    initial commit

--- jscript.js
+++ jscript.js
@@ -1,3 +1,27 @@
 
 
 
+
+
+	
+function klikon2() {
+    
+    var y = document.getElementById("form2");
+     setTimeout(function(){ y.style.display="block" }, 1500);
+};
+
+
+
+
+
+
+function klikon1() {
+  document.getElementById("nav1").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("nav1").style.width = "0%";
+}
+
+
+
